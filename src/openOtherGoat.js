5c09b562c7c6f4de063916d0164e1f0b1e25353f
bada308 <203190@jnu.ac.kr>
2023-05-01 20:40:43 +0900

    fix : openOther function 키워드로 수정

--- src/openOtherGoat.js
+++ src/openOtherGoat.js
@@ -1,7 +1,7 @@
-export const openOtherGoat = (userInput, carPosition) => {
+export function openOtherGoat(userInput, carPosition) {
   const door = [0, 1, 2];
   const openPosition = door.filter(
     (d) => d != userInput && d != carPosition
   )[0];
   return openPosition;
-};
+}
