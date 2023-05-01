b67ca8515118f8524b94902a088ce63567a30fff
bada308 <203190@jnu.ac.kr>
2023-05-01 20:32:44 +0900

    FEAT : 염소가 들어있는 문 보여주기

---
+++ src/openOtherGoat.js
@@ -0,0 +1,7 @@
+export const openOtherGoat = (userInput, carPosition) => {
+  const door = [0, 1, 2];
+  const openPosition = door.filter(
+    (d) => d != userInput && d != carPosition
+  )[0];
+  return openPosition;
+};
